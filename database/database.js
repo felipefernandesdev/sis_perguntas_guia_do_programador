b5dd08c0c76e1509ef6ff2567900a552a5f937a0
fesousadev <fesousalocutor@gmail.com>
2021-05-13 01:23:02 -0300

    up

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('perguntas', 'root', '@Meui302015',{
+const connection = new Sequelize('perguntas', 'root', '###########',{
     host: 'localhost',
     dialect: 'mysql'
 });

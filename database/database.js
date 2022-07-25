2921ff9012376540c83db1f154b9f493d296c973
Felipe Fernandes <fesousalocutor@gmail.com>
2022-07-25 14:33:19 -0300

    Update database.js
    update details database connect sequelize

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('perguntas', 'root', '###########',{
+const connection = new Sequelize('db_name', 'db_user', 'db_password',{
     host: 'localhost',
     dialect: 'mysql'
 });
